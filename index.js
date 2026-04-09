662a47daf80115db0f7c42cc116db02554aed10b
penzimbuthia-sudo <penzifavour@gmail.com>
2026-04-10 00:01:28 +0300

    Add functions to js file

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+function calculateTax(amount) {
+    let tax = amount * 0.10;
+    return tax;
+}
+console.log(calculateTax(100))
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+console.log(convertToUpperCase("hello world"));
 
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else{
+        return num2;
+    }
+}
+console.log(findMaximum(20, 68));
 
+function isPalindr(word) {
+    const reversedWord = word.split(' ').reverse().join(' ');
+    return word === reversedWord;
+}
+console.log(isPalindr("radar"));
+console.log(isPalindr("hello"))
+console.log(isPalindr("racecar"));
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discount = originalPrice * (discountPercentage / 100)
+    let finalPrice = originalPrice - discount;
+    return finalPrice;
+}
+let result = calculateDiscountedPrice(100, 15)
+console.log(result)
 
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindr}
